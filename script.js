753e1534d517431a6eb916f53ad6819f7a7594c8
baron5ss-maker <baron5ss@icloud.com>
2026-07-09 01:34:33 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("推し録 起動！");
+const home = document.getElementById("home-screen");
+const add = document.getElementById("add-screen");
+
+const buttons = document.querySelectorAll("nav button");
+
+buttons[1].addEventListener("click", () => {
+
+    home.style.display = "none";
+    add.style.display = "block";
+
+});
+
+buttons[0].addEventListener("click", () => {
+
+    add.style.display = "none";
+    home.style.display = "block";
+
+});
